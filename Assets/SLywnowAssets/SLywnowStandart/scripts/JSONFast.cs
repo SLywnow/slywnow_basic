824264ca5b6df9dd855d1d765d07608f1ec404a4
SLywnow <nicbor1998@gmail.com>
2023-03-05 19:06:57 +0300

    Added JSONFile as JSON parser

--- Assets/SLywnowAssets/SLywnowStandart/scripts/JSONFast.cs
+++ Assets/SLywnowAssets/SLywnowStandart/scripts/JSONFast.cs
@@ -314,7 +314,7 @@ namespace SLywnow
 		{
 			if (ss_sl == null) { if (useDebug) Debug.LogError("Use SaveSystemAlt.StartWork(); first"); return def; }
 
-			if (ss_sl.name.Contains(key)) if (ss_sl.type[ss_sl.name.IndexOf(key)] == SaveSystemSL.SSLTpe.floatS || ss_sl.type[ss_sl.name.IndexOf(key)] == SaveSystemSL.SSLTpe.undefinedS)
+			if (ss_sl.name.Contains(key)) if (ss_sl.type[ss_sl.name.IndexOf(key)] == SaveSystemSL.SSLTpe.floatS || ss_sl.type[ss_sl.name.IndexOf(key)] == SaveSystemSL.SSLTpe.intS || ss_sl.type[ss_sl.name.IndexOf(key)] == SaveSystemSL.SSLTpe.undefinedS)
 				{
 					try { return float.Parse(ss_sl.contain[ss_sl.name.IndexOf(key)]); }
 					catch { return def; }
@@ -630,4 +630,206 @@ namespace SLywnow
 		public string data;
 		public int type;
 	}
+
+	public class JSONFile
+	{
+		static int pt = -1;//parse at
+		static char[] toParse;
+		public string value;
+
+		public JSONFile parent = null;
+		public Dictionary<string, JSONFile> map;
+		public JSONFile this[string ind_]
+		{
+			get
+			{
+				if (map.ContainsKey(ind_))
+					return map[ind_];
+				else
+					return new JSONFile();
+			}//get
+		}
+		public bool has(params string[] path_)
+		{
+			Dictionary<string, JSONFile> m = map;
+			for (int i = 0; i < path_.Length; i++)
+			{
+				//Debug.Log(path_[i]+":"+(m!=null));
+				if (m == null || !m.ContainsKey(path_[i]))
+					return false;
+				m = m[path_[i]].map;
+			}
+			return true;
+		}
+
+		public string get(params string[] path_)
+		{
+			JSONFile m = this;
+			for (int i = 0; i < path_.Length; i++)
+			{
+				//Debug.Log(path_[i]+":"+(m!=null));
+				if (!m.map.ContainsKey(path_[i]))
+					return null;
+				m = m.map[path_[i]];
+			}
+			return m.value;
+		}
+
+		public string getAllKeys(params string[] path_)
+		{
+			JSONFile m = this;
+			for (int i = 0; i < path_.Length; i++)
+			{
+				//Debug.Log(path_[i]+":"+(m!=null));
+				if (!m.map.ContainsKey(path_[i]))
+					return null;
+				m = m.map[path_[i]];
+			}
+			return m.map.Keys.ToString();
+		}
+
+		public char valueType = 'v';//v:value o:object a:array
+		public JSONFile parse(string txt = "")
+		{
+			if (!string.IsNullOrEmpty(txt))
+			{
+				value = txt;
+				toParse = txt.ToCharArray();
+				pt = -1;
+			}
+			string key = "";
+			while (pt < toParse.Length)
+			{
+				//STEP 1:look for key 
+				switch (toParse[++pt])
+				{
+					case '{'://initialize object
+						valueType = 'o';
+						map = new Dictionary<string, JSONFile>();
+						continue;
+					//break;
+					case '['://initialize array
+						valueType = 'a';
+						map = new Dictionary<string, JSONFile>();
+						//TODO: exit this procedure and do array lookup
+						break;
+
+					//termination
+					//TODO:terminate object
+					case '}':
+						return this;//break;
+										  //TODO:terminate array
+					case ']':
+						return this;//break;
+										  //TODO: next item
+					case ',':
+						if (valueType == 'o')
+						{
+							key = "";
+							continue;
+						}
+						break;
+
+
+					//skip empty
+					case ' ': case '\n': case '\t': continue;
+
+
+					// (")
+					case '"':
+						key = "";
+						//get key name
+						while (toParse[++pt] != '"') key += toParse[pt];
+						//skip till colon
+						while (toParse[++pt] != ':') ;
+						break;
+					// (")
+					case '\'':
+						key = "";
+						//get key name
+						while (toParse[++pt] != '\'') key += toParse[pt];
+						//skip till colon
+						while (toParse[++pt] != ':') ;
+						break;
+
+					//keys that has no (")
+					default:
+						key = "";
+						pt--;
+						//Debug.Log("tt:"+toParse[pt]);
+						while (toParse[++pt] != ':')
+							if (toParse[pt] != ' ' && toParse[pt] != '\n' && toParse[pt] != '\t')
+								key += toParse[pt];
+						break;
+				}
+				//Debug.Log("Key>"+key);
+
+				//if no key found yet, keep looking
+				if (key == "" && valueType != 'a')
+					continue;
+				//prepare for array
+				if (valueType == 'a')
+				{
+					if (key == "")
+						key = "0";
+					else
+						key = (int.Parse(key) + 1).ToString();
+				}
+				//STEP 2: look for value,array,object
+
+				//skip empty area
+				do { pt++; }
+				while (toParse[pt] == ' ' || toParse[pt] == '\n'
+										|| toParse[pt] == '\t');
+				//pt--;
+				//Debug.Log("value is: "+toParse[pt]);
+				switch (toParse[pt])
+				{
+					case '{':
+					case '[':
+						pt--;
+						map[key] = new JSONFile().parse();
+						break;
+					case '"':
+						map[key] = new JSONFile();
+						map[key].value = "";
+						bool ignorenext=false;
+						while (toParse[++pt] != '"' || ignorenext)
+						{
+							ignorenext = false;
+							if (toParse[pt] == '\\')
+								ignorenext = true;
+							map[key].value += toParse[pt];
+						}
+						break;
+
+					case '\'':
+						map[key] = new JSONFile();
+						map[key].value = "";
+						while (toParse[++pt] != '\'')
+							map[key].value += toParse[pt];
+						break;
+
+					default:
+						map[key] = new JSONFile();
+						map[key].value = "";
+						pt--;
+						while (toParse[++pt] != ' '
+							&& toParse[pt] != '\n'
+							&& toParse[pt] != '\t'
+							&& toParse[pt] != ','
+							&& toParse[pt] != ']'
+							&& toParse[pt] != '}')
+							map[key].value += toParse[pt];
+						//
+						pt--;//next iteration needs to know it
+
+						continue;//break;
+				}
+			}
+
+			return this;
+		}
+	}
+
 }
